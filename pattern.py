c8216c7e9bd3c60fd8b4a09384eedaf2b103e630
Partzhh_9 <parthchotaliya788@gmail.com>
2025-07-04 11:52:04 +0530

    added some files

--- pattern.py
+++ pattern.py
@@ -6,10 +6,22 @@
 
 # print pattern with for loop
 
-for i in range(1,6):
-    for j in range(1,6 -i):
-        print("* ", end="")
-    print("*")
+# for i in range(1,6):
+#     for j in range(1,6 -i):
+#         print("* ", end="")
+#     print("*")
 
 
 # print pattern with for loop in conditional operator
+
+# for i in range(1, 6):
+#     for j in range(1, 6 - i):
+#         print("* ", end="")
+#     print("*" if i < 6 else "")
+
+# print pattern without nasted loop
+
+# for i in range(1,6):
+#     print("* " * (6 - i),end="")
+#     print("* " if i < 7 else "")
+
