b3ec0addc0de91982702a12177037cdad8632262
Partzhh_9 <parthchotaliya788@gmail.com>
2025-07-04 12:04:18 +0530

    added some files

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
-# age = int(input("Enter a number: "))
-# if (age < 18):
-#     print("You are teenager.")
-# elif (age > 18 and age < 60):
-#     print("You are an adult.")
-# else:
-#     print("You are a senior citizen.")
+age = int(input("Enter a number: "))
+if (age < 18):
+    print("You are teenager.")
+elif (age > 18 and age < 60):
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
 
-print("Hello World!")
+# print("Hello World!")
