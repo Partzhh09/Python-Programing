551a6500bae67c43ef45a897d35bac1a5ab161fe
Partzhh_9 <parthchotaliya788@gmail.com>
2025-09-03 10:15:37 +0530

    added some files

--- file.py
+++ file.py
@@ -1,3 +1,11 @@
-f = open("Data.txt", "r")
-data = f.readline()
+# f = open("Data.txt", "w")
+# data = f.write("Hello World")
+# print(data)
+
+# f = open("Data.txt", "r")
+# data = f.readlines()
+# print(data)
+
+f = open("Data.txt", "a")
+data = f.write("\nMy Name Is ....")
 print(data)
